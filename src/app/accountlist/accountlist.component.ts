62aa2d278d9f7499ef8da9f40d4cab2dd73b65e4
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-24 13:29:06 -0400

    update accountlist component

--- src/app/accountlist/accountlist.component.ts
+++ src/app/accountlist/accountlist.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accountlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accountlist.component.html',
   styleUrl: './accountlist.component.css'
 })
 export class AccountlistComponent {
+  accounts: string[] = [];
 
+  constructor() {
+    for (let i = 1; i <= 100; i++) {
+      this.accounts.push(`Account ${i}`);
+    }
+  }
 }
