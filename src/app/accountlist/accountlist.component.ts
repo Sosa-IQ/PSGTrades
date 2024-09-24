086bf823bf7f1ce5704b5e8d443caeee5574e048
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-24 13:15:22 -0400

    add accountlist component

---
+++ src/app/accountlist/accountlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accountlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './accountlist.component.html',
+  styleUrl: './accountlist.component.css'
+})
+export class AccountlistComponent {
+
+}
