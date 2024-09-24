1b2b22780cffc1991b53fdf1791d4eb95824ba32
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-24 13:29:47 -0400

    reconfigure dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AccountlistComponent } from '../accountlist/accountlist.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [AccountlistComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
