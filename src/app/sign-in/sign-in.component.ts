9ba5e7b88b97c347a4b5ad08cf42df00c0c7d7a8
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-24 12:51:35 -0400

    update submission in sign in form

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-
+  constructor(private router: Router) {}
+  
+  onSubmit(signInForm: NgForm) {
+    console.log('Sign in clicked');
+    if (signInForm.valid) {
+      console.log('Form is valid, navigating to dashboard');
+      this.router.navigate(['/dashboard']);
+    }
+  }
 }
