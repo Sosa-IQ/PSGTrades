4f861ed9fc96c456efc7c6b020d1f9d86098fd44
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-23 21:27:26 -0400

    add signup & signin routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
+  { path: 'signin', component: SignInComponent },
 ];
