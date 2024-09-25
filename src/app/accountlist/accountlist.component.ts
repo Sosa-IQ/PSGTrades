0f0955ee08631a87801fccc630cd6a474f74cb11
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-25 16:49:43 -0400

    Add api to request accounts

--- src/app/accountlist/accountlist.component.ts
+++ src/app/accountlist/accountlist.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-accountlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './accountlist.component.html',
   styleUrl: './accountlist.component.css'
 })
-export class AccountlistComponent {
+export class AccountlistComponent implements OnInit {
   accounts: string[] = [];
+  data: any[] = [];
 
-  constructor() {
-    for (let i = 1; i <= 100; i++) {
-      this.accounts.push(`Account ${i}`);
-    }
+  httpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.fetchAccounts();
+  }
+
+  fetchAccounts() {
+    this.httpClient
+      .get('https://25ikecna96.execute-api.us-east-1.amazonaws.com/Test/accounts')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.data = data;
+        this.accounts = data['accounts'];
+      });
   }
 }
