c03b5bcad74fafc669496eb4f027f043a5566633
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-25 17:30:49 -0400

    update buy/sell buttons

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { AccountlistComponent } from '../accountlist/accountlist.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AccountlistComponent],
+  imports: [AccountlistComponent, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  onBuy(orderForm: NgForm) {
+    console.log('Buy Button clicked');
+    if (orderForm.valid) {
+      console.log("Buying", orderForm.value);
+      orderForm.resetForm();
+    }
+  }
 
+  onSell(orderForm: NgForm) {
+    console.log('Sell Button clicked');
+    if (orderForm.valid) {
+      console.log("Selling", orderForm.value);
+      orderForm.resetForm();
+    }
+  }
 }
