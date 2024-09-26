77071c798d01e3f1af173dc0354eddee9c7940f5
Jancarlos Sosa <jsosa@Jancarloss-MacBook-Air.local>
2024-09-26 17:03:49 -0400

    add authentication component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { AccountlistComponent } from '../accountlist/accountlist.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { SignUpComponent } from "../sign-up/sign-up.component";
+import { CommonModule } from '@angular/common';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AccountlistComponent, FormsModule],
+  imports: [AccountlistComponent, FormsModule, SignUpComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  constructor(public authenticator: AuthenticatorService) {
+  }
+  
   onBuy(orderForm: NgForm) {
     console.log('Buy Button clicked');
     if (orderForm.valid) {
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,22 +1,50 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-
+import { RouterLink } from '@angular/router';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, AmplifyAuthenticatorModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  constructor(private router: Router) {}
   
-  onSubmit(signUpForm: NgForm) {
-    console.log('Sign up clicked');
-    if (signUpForm.valid) {
-      console.log('Form is valid, navigating to dashboard');
-      this.router.navigate(['/dashboard']);
-    }
-  }
+  // onSubmit(signUpForm: NgForm) {
+  //   console.log('Sign up clicked');
+  //   if (signUpForm.valid) {
+  //     console.log('Form is valid, navigating to dashboard');
+  //     this.router.navigate(['/dashboard']);
+  //   }
+  // }
+
+  name = '';
+
+  formFields = {
+    signUp: {
+      name: {
+        order: 1,
+        placeholder: 'Enter your full name'
+      },
+      email: {
+        order: 2
+      },
+      phone_number: {
+        order: 3
+      },
+      'custom:token': {
+        order: 4,
+        label: 'Access Token',
+        placeholder: 'Enter your full token',
+        isRequired: true
+      },
+      password: {
+        order: 5
+      },
+      confirm_password: {
+        order: 6
+      }
+    },
+  };
 }
